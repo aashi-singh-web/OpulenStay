7808e8701ef365e7a3b59e3ee3829a04926ccc1f
Aashi Singh <aashiishere.2004@gmail.com>
2025-07-05 00:27:29 +0530

    implemented the delete route

--- app.js
+++ app.js
@@ -69,6 +69,13 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
 
 
