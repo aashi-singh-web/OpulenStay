20aac8101e1fd6734fc819ef656cfef45b5f34d0
Aashi Singh <aashiishere.2004@gmail.com>
2025-07-04 23:20:27 +0530

    index route created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing'); // Importing the Listing model
 
+const path= require('path');
 
 // setting up the database
 const mongo_url = 'mongodb://127.0.0.1:27017/OpulenStay';
@@ -18,22 +19,34 @@ main()
    await mongoose.connect(mongo_url);
  };
 
+ app.set('view engine', 'ejs'); 
+ app.set('views', path.join(__dirname, 'views')); 
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get("/test", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new Villa",
-        description: "A beautiful villa with a pool",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",   
-    });
-    await sampleListing.save();
-    console.log("Sample listing saved to the database");
-    res.send("Sample listing saved to the database");
+
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+
+
+// app.get("/test", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "A beautiful villa with a pool",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",   
+//     });
+//     await sampleListing.save();
+//     console.log("Sample listing saved to the database");
+//     res.send("Sample listing saved to the database");
+// });
 
 
 app.listen(8080, () => {
