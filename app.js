79c08870bcc004e89dc3c3e3fc5493f8bce158e2
Aashi Singh <aashiishere.2004@gmail.com>
2025-07-05 00:06:34 +0530

    new route form and create route implemented

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ app.get("/listings/:id", async (req, res) => {
   res.render("listings/show.ejs", { listing });
 });
 
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
 
 
 // app.get("/test", async (req, res) => {
