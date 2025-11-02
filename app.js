36d74699473e70392bab6735936ae1c26b251ed9
Aashi Singh <aashiishere.2004@gmail.com>
2025-11-03 05:01:58 +0530

    done

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -69,7 +69,7 @@ const sessionOptions = {
 const store= MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
